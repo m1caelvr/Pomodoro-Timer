14df625178b140f8fa8d6c90899448d52a392e51
Micael-Rodrigues <micaelvitor709@gmail.com>
2024-08-15 23:44:53 -0300

    Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,49 @@
+import { Play } from "@phosphor-icons/react";
+import {
+    CountdownContainer,
+    FormContainer,
+    HomeContainer,
+    MinutesAmountInput,
+    Separator,
+    StartCountdownButton,
+    TaskInput
+} from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput
+                        placeholder="Dê um nome para o seu projeto"
+                        type="text"
+                        id="task"
+                    />
+
+                    <label htmlFor="minutesAmount"></label>
+                    <MinutesAmountInput
+                        id="minutesAmount"
+                        placeholder="00"
+                        type="number"
+                    />
+
+                    <span>minutos.</span>
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton type="submit">
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
